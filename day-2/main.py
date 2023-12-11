df9d645f2ac3444e7789a1996302b306a549d646
noahw <noah.wallentin@icloud.com>
2023-12-11 17:48:27 +0100

    Day 2 | Part 2 (forgot regex licensing lol)

--- day-2/main.py
+++ day-2/main.py
@@ -2,7 +2,9 @@ import re
 
 
 def max_cubes(line: str, comparison_color: str) -> int:
-    cubes = re.findall(r"(\d+) (\w+)", line)
+    cubes = re.findall(
+        r"(\d+) (\w+)", line
+    )  # i dont want to use 10000 splits lol (https://regexlicensing.org/)
     return max(int(count) for count, color in cubes if color == comparison_color)
 
 
